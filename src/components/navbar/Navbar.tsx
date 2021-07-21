4459bdf4b3751a6ffa62d57022b9dd463ee1bb32
ajgi123 <igorszukalo@gmail.com>
2021-07-21 21:52:30 +0200

    Add Navbar Component

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,109 @@
+import AppBar from "@material-ui/core/AppBar";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Brightness3Icon from "@material-ui/icons/Brightness3";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import { useState } from "react";
+import { useAuth } from "../../context/AuthProvider";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      display: "none",
+      [theme.breakpoints.up("sm")]: {
+        display: "block",
+      },
+    },
+  })
+);
+
+type NavbarPropsType = {
+  isDarkMode: boolean;
+  toggleHandler: () => void;
+};
+
+export default function Navbar(props: NavbarPropsType) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const { isAuthenticated, userData, logOutUser } = useAuth();
+  const displayName = userData?.displayName;
+  const email = userData?.email;
+
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem
+        onClick={() => {
+          logOutUser();
+          setAnchorEl(null);
+        }}
+      >
+        Log out
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="fixed" color="default">
+        <Toolbar>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Todo App
+          </Typography>
+          <div className={classes.grow} />
+          <div>
+            <IconButton onClick={props.toggleHandler}>
+              {props.isDarkMode ? <Brightness4Icon /> : <Brightness3Icon />}
+            </IconButton>
+            {isAuthenticated && (
+              <>
+                {displayName || email}
+                <IconButton
+                  edge="end"
+                  aria-label="account of current user"
+                  aria-controls={menuId}
+                  aria-haspopup="true"
+                  onClick={handleProfileMenuOpen}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+              </>
+            )}
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMenu}
+    </div>
+  );
+}
