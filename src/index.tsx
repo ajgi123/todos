f2e5eaa131ff3f3b328a834c7a496d325d112acb
ajgi123 <igorszukalo@gmail.com>
2021-07-21 22:08:07 +0200

    Change index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AuthProvider } from "./context/AuthProvider";
 
+const queryClient = new QueryClient();
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <BrowserRouter basename="/todos">
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-
